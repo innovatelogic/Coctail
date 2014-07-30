e859ec100b04034b3b2e297ec291f13e13bfc2ac
YurG <yura.gunko@gmail.com>
2014-07-31 02:49:16 +0300

    ajax request

---
+++ assets/www/php/test.php
@@ -0,0 +1,7 @@
+<?php
+	header('Content-type: application/json');
+	header("Access-Control-Allow-Origin: *");
+	//echo 'blablabla';
+	echo json_encode('blablabla');
+	//echo "2014-6-31-1,2014-7-1-2,2014-7-3-2,2014-7-15-2";
+?>
